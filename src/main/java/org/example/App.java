ca9c6bb03b0f08c288f58ba7953c82bae0c17382
igor-chest <shesterin51@gmail.com>
2023-04-13 21:54:27 +0500

    create db and person model class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,33 @@
 package org.example;
 
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 1); // получаем сущность по id через сессию из бд через транзакцию
+            System.out.println(person.getName());
+            System.out.println(person.getAge());
+
+            session.getTransaction().commit(); // закрываем транзакцию
+        } finally {
+            sessionFactory.close();
+        }
+
     }
 }
