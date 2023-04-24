757ba21e7079a48901120e1a47661b58de2cbf54
igor-chest <shesterin51@gmail.com>
2023-04-24 14:56:39 +0500

    many to many

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,37 +1,40 @@
 package org.example;
 
-import org.example.model.Passport;
-import org.example.model.Person;
+import org.example.model.Actor;
+import org.example.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 public class App {
     public static void main( String[] args ) {
 
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
 
-        try {
+
+        try (sessionFactory) { // try с ресурсами после выполнения сам закроет sessionFactory
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Person person = new Person("Dima", 22);
-            Passport passport = new Passport(456234);
+            Actor actor = session.get(Actor.class, 2);
+            System.out.println(actor.getMovies());
 
-            person.setPassport(passport);
+            Movie movie = actor.getMovies().get(0);
 
-            session.save(person);
+            actor.getMovies().remove(0);
+            movie.getActors().remove(actor);
 
             session.getTransaction().commit(); // закрываем транзакцию
 
-        } finally {
-            sessionFactory.close();
         }
-
     }
 }
